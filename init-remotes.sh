4f93781661d4616ffda533e3341f79528e4ff3c8
Danny Baumann <dannybaumann@web.de>
2011-06-02 12:49:24 +0200

    Some more script fixes.
    Change-Id: I10a11256273b3d938cc8b620333e96a2c278e088

--- init-remotes.sh
+++ init-remotes.sh
@@ -11,22 +11,26 @@ function add_remote()
 		elif [ "$r" = "review" ]; then
 			url="ssh://review.cyanogenmod.com:29418/CyanogenMod"
 		fi
+		git remote rm $r
 		git remote add $r $url/$2.git
 	done
+	if [ -n "$4" ]; then
+		git checkout --track -b $4 myrepos/$4
+	fi
 	popd
 }
 
 if [ $# -ne 0 ]; then
-	add_remote $1 $2 $3
+	add_remote $1 $2 $3 $4
 	exit 0
 fi
 
-add_remote vendor/cyanogen android_vendor_cyanogen "quarx cyanogen review"
-add_remote android android "cyanogen"
-add_remote device/motorola/jordan android_device_motorola_umts_jordan "quarx"
-add_remote frameworks/base android_frameworks_base "quarx cyanogen review"
-add_remote hardware/ti android_hardware_ti "quarx"
-add_remote packages/apps/Phone android_packages_apps_Phone "quarx cyanogen review"
+add_remote vendor/cyanogen android_vendor_cyanogen "quarx cyanogen review" gingerbread
+add_remote android android "cyanogen" gingerbread
+add_remote device/motorola/jordan android_device_motorola_umts_jordan "quarx" gingerbread
+add_remote frameworks/base android_frameworks_base "quarx cyanogen review" gingerbread
+add_remote hardware/ti android_hardware_ti "quarx" master
+add_remote packages/apps/Phone android_packages_apps_Phone "quarx cyanogen review" gingerbread
 add_remote packages/apps/Camera android_packages_apps_Camera "cyanogen"
 add_remote packages/apps/Settings android_packages_apps_Settings "cyanogen"
 add_remote packages/apps/Torch android_packages_apps_Torch "cyanogen"
--- update-remotes.sh
+++ update-remotes.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-function merge_upstream()
-{
-	git fetch $1
-	git merge remotes/$1/gingerbread
-}
-
 function do_package()
 {
 	pushd $1
 	for r in $2; do
 		git fetch $r
-		git merge $r/gingerbread
+		git merge $r/$3
 	done
-	if [ "$3" = "push" ]; then
+	if [ "$4" = "push" ]; then
 		git push
 	fi
 	popd
 }
 
-do_package android "cyanogen" push
-do_package device/motorola/jordan "quarx" push
-do_package frameworks/base "cyanogen quarx" push
-do_package hardware/ti "quarx" push
-do_package packages/apps/Phone "cyanogen quarx" push
-do_package vendor/cyanogen "cyanogen quarx" push
-do_package packages/apps/Camera "cyanogen"
-do_package packages/apps/Settings "cyanogen"
-do_package packages/apps/Torch "cyanogen"
+do_package android "cyanogen" gingerbread push
+do_package device/motorola/jordan "quarx" gingerbread push
+do_package frameworks/base "cyanogen quarx" gingerbread push
+do_package hardware/ti "quarx" master push
+do_package packages/apps/Phone "cyanogen quarx" gingerbread push
+do_package vendor/cyanogen "cyanogen quarx" gingerbread push
+do_package packages/apps/Camera "cyanogen" gingerbread
+do_package packages/apps/Settings "cyanogen" gingerbread
+do_package packages/apps/Torch "cyanogen" gingerbread
 
